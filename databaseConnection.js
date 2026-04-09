84884e815dbdb1e7a2da11b937a189a223548081
Calvin Mwin-Era <45calvin45@gmail.com>
2026-04-08 23:19:42 -0700

    updated db variable

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,8 +1,8 @@
 const database = require("mongoose");
 const is_hosted = process.env.IS_HOSTED || false;
-// const databaseName = "lab_example"
-const hostedURI = `mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/${process.env.REMOTE_MONGODB_DATABASE}?retryWrites=true&w=majority`
-const localURI = `mongodb://localhost/${process.env.REMOTE_MONGODB_DATABASE}?authSource=admin&retryWrites=true`
+const databaseName = "lab_example"
+const hostedURI = `mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/${databaseName}?retryWrites=true&w=majority`
+const localURI = `mongodb://localhost/${databaseName}?authSource=admin&retryWrites=true`
 if (is_hosted) {
 database.connect(hostedURI, {useNewUrlParser: true, useUnifiedTopology: true});
 }
