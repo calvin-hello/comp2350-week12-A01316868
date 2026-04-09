c9e25ba700e00bfe5e416b6a2db68fbc13565a05
Calvin Mwin-Era <45calvin45@gmail.com>
2026-04-09 04:33:59 -0700

    modified connection string

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,14 +1,39 @@
-const database = require("mongoose");
-const is_hosted = process.env.IS_HOSTED || false;
-const databaseName = "lab_example"
-const hostedURI = `mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/${databaseName}?retryWrites=true&w=majority`
-const localURI = `mongodb://localhost/${databaseName}?authSource=admin&retryWrites=true`
-if (is_hosted) {
-database.connect(hostedURI, {useNewUrlParser: true, useUnifiedTopology: true});
-}
-else {
-database.connect(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
-}
+const mongoose = require("mongoose");
+
+const databaseName = "lab_example";
+const is_hosted = process.env.IS_HOSTED === "true";
+
+const hostedURI = `mongodb+srv://${process.env.REMOTE_MONGODB_USER}:${encodeURIComponent(process.env.REMOTE_MONGODB_PASSWORD)}@${process.env.REMOTE_MONGODB_HOST}/${process.env.REMOTE_MONGODB_DATABASE || databaseName}?retryWrites=true&w=majority`;
+
+const localURI = `mongodb://127.0.0.1:27017/${databaseName}`;
+
+const uri = is_hosted ? hostedURI : localURI;
+
+console.log("IS_HOSTED:", process.env.IS_HOSTED);
+console.log("Using database:", is_hosted ? "Atlas" : "Local MongoDB");
+
+mongoose.connect(uri)
+	.then(() => {
+		console.log("MongoDB connected");
+	})
+	.catch((err) => {
+		console.log("MongoDB connection error");
+		console.log(err);
+	});
+
+module.exports = mongoose;
+
+// const database = require("mongoose");
+// const is_hosted = process.env.IS_HOSTED || false;
+// const databaseName = "lab_example"
+// const hostedURI = `mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/${databaseName}?retryWrites=true&w=majority`
+// const localURI = `mongodb://localhost/${databaseName}?authSource=admin&retryWrites=true`
+// if (is_hosted) {
+// 	database.connect(hostedURI, {useNewUrlParser: true, useUnifiedTopology: true});
+// }
+// else {
+// 	database.connect(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
+// }
 
 
 
