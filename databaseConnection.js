9ee21e5854ab391172ff41aa950882aca5407396
Calvin Mwin-Era <45calvin45@gmail.com>
2026-04-09 04:55:31 -0700

    update db name in connection string

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,7 +1,7 @@
 const database = require("mongoose");
 const is_hosted = process.env.IS_HOSTED || false;
 const databaseName = "lab_example"
-const hostedURI = `mongodb+srv://${process.env.REMOTE_MONGODB_USER}:${process.env.REMOTE_MONGODB_PASSWORD}@cluster0.4ulcc.mongodb.net/${databaseName}?retryWrites=true&w=majority`
+const hostedURI = `mongodb+srv://${process.env.REMOTE_MONGODB_USER}:${process.env.REMOTE_MONGODB_PASSWORD}@${process.env.REMOTE_MONGODB_HOST}/${databaseName}?retryWrites=true&w=majority`
 const localURI = `mongodb://localhost/${databaseName}?authSource=admin&retryWrites=true`
 if (is_hosted) {
 	database.connect(hostedURI, {useNewUrlParser: true, useUnifiedTopology: true});
